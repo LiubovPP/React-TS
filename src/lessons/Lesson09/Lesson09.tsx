1ebf5310e293cbbf52bc3b985115154bba53cacc
Liubov Peshkova <maximliuba2@gmail.com>
2024-09-20 11:18:02 +0200

    add useEffect

--- src/lessons/Lesson09/Lesson09.tsx
+++ src/lessons/Lesson09/Lesson09.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { 
   InputExample,
   Lesson09Component,
@@ -8,17 +8,41 @@ import {
 function Lesson09() {
 
   const [inputValue, setInputValue]=useState<string>('');
+  const [inputValue2, setInputValue2] = useState<string>('');
 
   const onChangeInput = (event: ChangeEvent<HTMLInputElement  >)=>{
-  setInputValue(event.target.value);
-  }
+    setInputValue(event.target.value);
+    }  
+  const onChangeInput2 = (event: ChangeEvent<HTMLInputElement  >)=>{
+      setInputValue2(event.target.value);
+      }
 
- console.log(inputValue);
+  // console.log(inputValue); 
+
+  // Вызов функции callback при создании компонента. Пустой массив завсимостей
+  useEffect(()=> {
+  console.log('mounting');
+  }, []);
+  
+    // Вызов функции callback при обновлении компонента.  Массив завсимостей не пустой
+    useEffect(() => {
+      console.log('Updating');
+    }, [inputValue]);
+
+ // Вызов функции callback при размонтировании компонента. 
+  //Массив завсимостей пустой, внутри callback функции возвращается другая функция
+  useEffect(() => {
+    return () => {
+      console.log('Unmounting');
+    }
+  }, []);
 
   return <Lesson09Component >
   
     <InputExample name="example" placeholder="example text" onChange={onChangeInput}/>
+    <InputExample name='example2' placeholder='example text2' onChange={onChangeInput2} />
     <Result>{inputValue}</Result>
+    <Result>{inputValue2}</Result>
     </Lesson09Component>
 }
 
